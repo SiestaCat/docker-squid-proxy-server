2915c927fa0e4f68bdd8aa8f09c1e5bd2c983d66
Marco <marco.isfc@gmail.com>
2025-01-18 10:40:48 +0100

    Fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
+# Ensure log directory exists
+mkdir -p /var/log/squid /squid-proxy/cache
+chown squid:squid /var/log/squid
+chown squid:squid /squid-proxy/cache
+
 # Generate password file
 htpasswd -bc /etc/squid/passwd $SQUID_USER $SQUID_PASSWORD
 
-# Optionally configure the network interface
+# Configure the network interface
 if [ -n "$NETWORK_INTERFACE" ]; then
     echo "http_port $NETWORK_INTERFACE:3128" >> /etc/squid/squid.conf
 else
     echo "http_port 3128" >> /etc/squid/squid.conf
 fi
 
-# Start Squid in the background
-squid -N &
+# Start Squid in the foreground for better Docker integration
+squid -N -d 1
 
-# Tail the Squid logs to the console
-tail -f /var/log/squid/access.log
+# Follow the access log (in background to allow squid to run in foreground)
+tail -f /var/log/squid/access.log &
